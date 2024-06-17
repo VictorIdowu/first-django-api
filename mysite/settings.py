ee23bb187b0d1ce6052956035f9b45bddcac1ed8
VictorIdowu <idsonvictor@gmail.com>
2024-06-17 13:58:18 +0100

    kk

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,6 +131,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = (
-  'http://localhost:3000',
+  'http://localhost:5173',
   'http://localhost:8000'
 )
